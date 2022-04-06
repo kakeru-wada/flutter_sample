c45f30ddffdbeaac77030730eaae2a64ec017f79
kakeruwada <wadakkr@bizsoft.biz>
2022-04-06 18:12:28 +0900

    UIを色々いじった

--- lib/src/first_sample/main.dart
+++ lib/src/first_sample/main.dart
@@ -18,7 +18,7 @@ class FirstSampleApp extends StatelessWidget {
         // or simply save your changes to "hot reload" in a Flutter IDE).
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.red,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
@@ -56,6 +56,8 @@ class _MyHomePageState extends State<MyHomePage> {
       _counter++;
     });
   }
+  final List<String> icons = <String>['add', 'favorite'];
+  final List<String> colors = <String>['pink', 'blue'];
 
   @override
   Widget build(BuildContext context) {
@@ -70,42 +72,57 @@ class _MyHomePageState extends State<MyHomePage> {
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
+        actions: <Widget>[
+          IconButton(
+            onPressed: (){},
+            icon: const Icon(Icons.more_vert),
+          )
+        ],
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      body: Container(
+        constraints: const BoxConstraints.expand(),
+          decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: NetworkImage('https://www.nintendo.co.jp/switch/arzga/assets/img/copy/bg_copy.jpg'),
+                fit: BoxFit.cover,
+              )
+          ),
+          child: Column(
+            // Column is also a layout widget. It takes a list of children and
+            // arranges them vertically. By default, it sizes itself to fit its
+            // children horizontally, and tries to be as tall as its parent.
+            //
+            // Invoke "debug painting" (press "p" in the console, choose the
+            // "Toggle Debug Paint" action from the Flutter Inspector in Android
+            // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
+            // to see the wireframe for each widget.
+            //
+            // Column has various properties to control how it sizes itself and
+            // how it positions its children. Here we use mainAxisAlignment to
+            // center the children vertically; the main axis here is the vertical
+            // axis because Columns are vertical (the cross axis would be
+            // horizontal).
+
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              const Image(
+                image:NetworkImage(
+                  'https://www.nintendo.co.jp/switch/arzga/assets/img/copy/chara_about.png',
+                ),
+                width: 200,
+              ),
+              Text(
+                '$_counter',
+                style: const TextStyle(fontSize: 36, color: Colors.white),
+              ),
+              IconButton(
+                onPressed: _incrementCounter,
+                color: Colors.pink,
+                icon: const Icon(Icons.favorite),
+              ),
+            ],
+          ),
+        )
     );
   }
 }
