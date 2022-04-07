0a08aa8fd6d14e33da73904a77b44a4fd647dacb
tsurudahryk <tsurudahryk@bizsoft.co.jp>
2022-04-07 12:50:11 +0900

    アプリ画面切替

--- lib/main.dart
+++ lib/main.dart
@@ -2,5 +2,6 @@ import 'package:flutter/material.dart';
 import 'src/export.dart';
 
 void main() {
-  runApp(const FirstSampleApp());
+  //runApp(const FirstSampleApp());
+  runApp(ListViewApp());
 }
--- lib/src/export.dart
+++ lib/src/export.dart
@@ -1,1 +1,2 @@
 export 'first_sample/main.dart';
+export 'sample_list_view/list_view.dart';
